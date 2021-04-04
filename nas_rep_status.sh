79f6940288365b4e9509ca7efa0a8b7bfb621925
Ingo <foren@asche-rz.de>
2021-04-04 16:04:15 +0200

    Update nas_rep_status.sh

--- nas_rep_status.sh
+++ nas_rep_status.sh
@@ -9,8 +9,10 @@ if [[ ! -z "$SHAREREPS" ]]
 then
 for SHAREREP in "${SHAREREPS[@]}"
 do
-#echo $SHAREREP
 CONTENT=`cat $LOG | grep -E 'folder replication|$SHAREREP' | tail -1`
+if [ -z "${CONTENT}" ]; then
+	CONTENT=`cat $LOG | grep -E 'folder replication|$SHAREREP' | tail -1`
+fi
 if [[ $CONTENT == *"completed"* ]]; then
 	STATUS="1"
 	elif [[ $CONTENT == *"Failed"* ]]; then
@@ -22,7 +24,7 @@ ACTTIME=`date +%s`
 TIME=`echo $CONTENT | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}"`
 TIMEEND=`date -d "$TIME" +%s`
 LASTRUN=$(($ACTTIME - $TIMEEND))
-echo "<result><channel>Share $SHAREREP</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>Share $SHAREREP Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result>"
+echo "<result><channel>Share $SHAREREP: Last status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>Share $SHAREREP: Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result>"
 done
 fi
 
@@ -31,6 +33,9 @@ then
 for LUNREP in "${LUNREPS[@]}"
 do
 CONTENT=`cat $LOG | grep -E 'folder replication|$LUNREP' | tail -1`
+if [ -z "${CONTENT}" ]; then
+	CONTENT=`cat $LOG | grep -E 'folder replication|$LUNREP' | tail -1`
+fi
 if [[ $CONTENT == *"completed"* ]]; then
 	STATUS="1"
 	elif [[ $CONTENT == *"Failed"* ]]; then
@@ -42,7 +47,7 @@ ACTTIME=`date +%s`
 TIME=`echo $CONTENT | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}"`
 TIMEEND=`date -d "$TIME" +%s`
 LASTRUN=$(($ACTTIME - $TIMEEND))
-echo "<result><channel>LUN $LUNREP</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>LUN $LUNREP Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result>"
+echo "<result><channel>LUN $LUNREP: Last status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>LUN $LUNREP: Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result>"
 done
 fi
 echo "</prtg>"
