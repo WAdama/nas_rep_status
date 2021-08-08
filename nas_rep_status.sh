defa61ae7909c11df935ca355b68020735d3c258
Ingo <foren@asche-rz.de>
2021-08-08 18:24:45 +0200

    Got rid of the oldstyle backticks

--- nas_rep_status.sh
+++ nas_rep_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.0.0
+# Version 1.0.1
 
 CONF=$1
 source $CONF
@@ -9,9 +9,9 @@ if [[ ! -z "$SHAREREPS" ]]
 then
 for SHAREREP in "${SHAREREPS[@]}"
 do
-CONTENT=`cat $LOG | grep -F 'folder replication' | grep -F "$SHAREREP" | tail -1`
+CONTENT=$(cat $LOG | grep -F 'folder replication' | grep -F "$SHAREREP" | tail -1)
 if [ -z "${CONTENT}" ]; then
-	CONTENT=`cat $LOG | grep -F 'folder replication' | grep -F $SHAREREP | tail -1`
+	CONTENT=$(cat $LOG | grep -F 'folder replication' | grep -F $SHAREREP | tail -1)
 fi
 if [[ $CONTENT == *"completed"* ]]; then
 	STATUS="1"
@@ -20,9 +20,9 @@ if [[ $CONTENT == *"completed"* ]]; then
 	else
 	STATUS="0"
 fi
-ACTTIME=`date +%s`
-TIME=`echo $CONTENT | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}"`
-TIMEEND=`date -d "$TIME" +%s`
+ACTTIME=$(date +%s)
+TIME=$(echo $CONTENT | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
+TIMEEND=$(date -d "$TIME" +%s)
 LASTRUN=$(($ACTTIME - $TIMEEND))
 echo "<result><channel>Share $SHAREREP: Last status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>Share $SHAREREP: Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result>"
 done
@@ -32,9 +32,9 @@ if [[ ! -z "$LUNREPS" ]]
 then
 for LUNREP in "${LUNREPS[@]}"
 do
-CONTENT=`cat $LOG | grep -F 'LUN replication' | grep -F $LUNREP | tail -1`
+CONTENT=$(cat $LOG | grep -F 'LUN replication' | grep -F $LUNREP | tail -1)
 if [ -z "${CONTENT}" ]; then
-	CONTENT=`cat $LOG | grep -F 'LUN replication' | grep -F $LUNREP | tail -1`
+	CONTENT=$(cat $LOG | grep -F 'LUN replication' | grep -F $LUNREP | tail -1)
 fi
 if [[ $CONTENT == *"completed"* ]]; then
 	STATUS="1"
@@ -43,9 +43,9 @@ if [[ $CONTENT == *"completed"* ]]; then
 	else
 	STATUS="0"
 fi
-ACTTIME=`date +%s`
-TIME=`echo $CONTENT | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}"`
-TIMEEND=`date -d "$TIME" +%s`
+ACTTIME=$(date +%s)
+TIME=$(echo $CONTENT | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")
+TIMEEND=$(date -d "$TIME" +%s)
 LASTRUN=$(($ACTTIME - $TIMEEND))
 echo "<result><channel>LUN $LUNREP: Last status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>LUN $LUNREP: Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result>"
 done
