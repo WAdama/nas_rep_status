aca6c673c90475262b5a8a161a9f1e8127887d2c
Ingo <foren@asche-rz.de>
2024-07-20 21:45:57 +0200

    Update nas_lun_rep_status.sh
    Changed LUN name detection to synowebapi
    Minor corrections

--- nas_lun_rep_status.sh
+++ nas_lun_rep_status.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#Version 2.0.2
+#Version 2.0.3
 
+API=$(which synowebapi)
 mapfile -t LUNREPS < <(sqlite3 /volume1/@appconf/SnapshotReplication/replica.db "select target_id,plan_id from plan where target_type like '1'")
 mapfile -t SYNODRLOGS < <( ls -1r /var/log/synolog/synodr.*[!.xz] )
 echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
 for LUNREP in "${LUNREPS[@]}"
 do
 IFS="|" read -r LUNUUID PLAN <<< "$LUNREP"
-LUNNAME=$(grep -wns "$LUNUUID" /volume1/@iSCSI/LUN/iscsi_lun.conf -B 1 | head -n 1 | cut -d "=" -f2)
+LUNNAME=$($API -s --exec api=SYNO.Core.ISCSI.LUN version=1 method=list | jq -r '.data.luns[] | select(.uuid=="'"$LUNUUID"'")' | jq -r .name)
+
 if [ -z "$PLAN" ] 
 then
   LASTRUN=""
@@ -17,7 +19,7 @@ else
   STATUS="2"
   if [ -f "/volume1/@appconf/SnapshotReplication/plan/$PLAN/sync_report" ]
   then
-    mapfile -t RESULT < <(jq .recent_records[-1] < /volume1/@appconf/SnapshotReplication/plan/"$PLAN"/sync_report | jq -r .begin_time,.finish_time,.is_success,.sync_size_byte)
+    mapfile -t RESULT < <(jq .recent_records[-1] < /volume1/@appconf/SnapshotReplication/plan/"$PLAN"/sync_report | jq -r .begin_time,.finish_time,.sync_size_byte)
     CONTENT=$(awk "/iSCSI LUN/ && /replication/ && /${LUNNAME}/" "${SYNODRLOGS[@]}" | tail -1)
     TIME=$(date -d "$(echo "$CONTENT" | grep -o "[0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\ [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}")" +%s)
     case $CONTENT in
@@ -30,10 +32,10 @@ else
     LASTRUN=$(("$ACTTIME"-"$TIME"))
     LASTSUCCESSRUN=$(("$ACTTIME"-"${RESULT[1]}"))
     RUNTIME=$(("${RESULT[1]}"-"${RESULT[0]}"))
-    SPEED=$(("${RESULT[3]}"/"$RUNTIME"))
+    SPEED=$(("${RESULT[2]}"/"$RUNTIME"))
   fi
 fi
-echo "<result><channel>LUN $LUNNAME: Last status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>LUN $LUNNAME: Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>LUN $LUNNAME: Last successful replication</channel><value>$LASTSUCCESSRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>LUN $LUNNAME: Runtime</channel><value>$RUNTIME</value><unit>TimeSeconds</unit></result><result><channel>LUN $LUNNAME: Data replicated</channel><value>${RESULT[3]}</value><unit>BytesDisk</unit><VolumeSize>MegaByte</VolumeSize></result><result><channel>LUN $LUNNAME: Speed</channel><value>$SPEED</value><unit>SpeedDisk</unit><SpeedSize>MegaByte</SpeedSize></result>"
+echo "<result><channel>LUN $LUNNAME: Last status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.repstatus</ValueLookup><ShowChart>0</ShowChart></result><result><channel>LUN $LUNNAME: Last run</channel><value>$LASTRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>LUN $LUNNAME: Last successful replication</channel><value>$LASTSUCCESSRUN</value><unit>TimeSeconds</unit><LimitMode>1</LimitMode><LimitMaxWarning>129600</LimitMaxWarning><LimitMaxError>216000</LimitMaxError></result><result><channel>LUN $LUNNAME: Runtime</channel><value>$RUNTIME</value><unit>TimeSeconds</unit></result><result><channel>LUN $LUNNAME: Data replicated</channel><value>${RESULT[2]}</value><unit>BytesDisk</unit><VolumeSize>MegaByte</VolumeSize></result><result><channel>LUN $LUNNAME: Speed</channel><value>$SPEED</value><unit>SpeedDisk</unit><SpeedSize>MegaByte</SpeedSize></result>"
 done
 echo "</prtg>"
 exit
